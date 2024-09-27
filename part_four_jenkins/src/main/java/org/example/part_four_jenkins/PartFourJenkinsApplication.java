a75cec1e2304ea1ee90ff320109247d92cf609dd
ahurein-amalitech <ebenezer.ahurein@amalitech.com>
2024-09-27 16:00:14 +0000

    feat: add jenkins

---
+++ part_four_jenkins/src/main/java/org/example/part_four_jenkins/PartFourJenkinsApplication.java
@@ -0,0 +1,13 @@
+package org.example.part_four_jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartFourJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartFourJenkinsApplication.class, args);
+    }
+
+}
---
+++ part_four_jenkins/src/test/java/org/example/part_four_jenkins/PartFourJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.part_four_jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartFourJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
